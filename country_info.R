6af158e8364b916cf705770da833d58cbece7f08
imranrhem <imran.rhemtulla@gmail.com>
2022-08-08 14:49:28 -0400

    created loop to add most recent information on different indicators

---
+++ country_info.R
@@ -0,0 +1,41 @@
+library(dplyr)
+library(httr)
+library(jsonlite)
+library(wbstats)
+
+##### Extract data from the worldbank database
+
+new_cache <- wb_cache()
+
+search_new <- wb_search("literacy rate")
+
+search_new %>% print(n = Inf)
+
+# Create a list of indicators
+my_indicators = c("gni_percapita" = "NY.GNP.PCAP.CD", #gross national income
+                  "gdp" = "NY.GDP.MKTP.CD",
+                  "gdp_percapita" = "NY.GDP.PCAP.CD",
+                  "gdp_growth" = "NY.GDP.MKTP.KD.ZG",
+                  "life_expectancy_birth" = "SP.DYN.LE00.IN",
+                  "infant_mortality_rate" = "SP.DYN.IMRT.IN",
+                  "human_development" = "UNDP.HDI.XD",
+                  "human_capital_index" = "HD.HCI.OVRL",
+                  "gender_equality" = "5.51.01.07.gender", # ratio of females to males that are educated
+                  "unemployment" = "SL.UEM.TOTL.ZS",
+                  # "poverty_gap" = "SI.POV.NGAP", gives 400 error may have to find another indicator
+                  # "poverty_rate" = "SI.POV.NAPR.ZS", gives 400 error may have to find another indicator
+                  # "literacy_rate" = "IN.POV.LIT.RAT.TOTL", gives 400 error may have to find another indicator
+                  "gini" = "SI.POV.GINI")
+
+# Create a new data frame that has all of the countries based off of population data availability
+country_indicators <- wb_data(my_indicators[1], country = "countries_only", mrnev = 1, freq = "Y")
+
+# Remove columns that are not important for analysis
+country_indicators <- country_indicators %>% select(-c(date, obs_status, footnote, last_updated))
+
+for (i in seq(1, length(my_indicators))){
+  indicator_data <- wb_data(my_indicators[i], country = "countries_only", mrnev = 1, freq = "Y")
+  country_indicators <- merge(country_indicators, indicator_data[c(3,5)], by = "country", all.x = TRUE)
+}
+
+
