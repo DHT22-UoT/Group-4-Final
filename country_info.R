48504814b26cc991dc739c424df34c53fb7aa984
imranrhem <imran.rhemtulla@gmail.com>
2022-08-09 11:31:23 -0400

    added different indicators

--- country_info.R
+++ country_info.R
@@ -3,29 +3,28 @@ library(httr)
 library(jsonlite)
 library(wbstats)
 library(countrycode)
+library(sys)
 
 ##### Extract data from the worldbank database
 
-new_cache <- wb_cache()
-
-search_new <- wb_search("population")
+search_new <- wb_search("literacy rate")
 
 search_new %>% print(n = Inf)
 
-# Create a list of indicators
+# Create a list of indicators of interest
 my_indicators = c("gni_percapita" = "NY.GNP.PCAP.CD", #gross national income
                   "gdp" = "NY.GDP.MKTP.CD",
                   "gdp_percapita" = "NY.GDP.PCAP.CD",
                   "gdp_growth" = "NY.GDP.MKTP.KD.ZG",
                   "life_expectancy_birth" = "SP.DYN.LE00.IN",
+                  "poverty_gap" = "SI.POV.UMIC.GP",
                   "infant_mortality_rate" = "SP.DYN.IMRT.IN",
                   "human_development" = "UNDP.HDI.XD",
                   "human_capital_index" = "HD.HCI.OVRL",
                   "gender_equality" = "5.51.01.07.gender", # ratio of females to males that are educated
                   "unemployment" = "SL.UEM.TOTL.ZS",
-                  # "poverty_gap" = "SI.POV.NGAP", # gives 400 error may have to find another indicator
-                  # "poverty_rate" = "SI.POV.NAPR.ZS", # gives 400 error may have to find another indicator
-                  # "literacy_rate" = "IN.POV.LIT.RAT.TOTL", # gives 400 error may have to find another indicator
+                  "literacy_rate" = "UIS.LR.AG25T64",
+                  "poverty_headcount_ratio" = "SI.POV.DDAY", # gives 400 error may have to find another indicator
                   "gini" = "SI.POV.GINI")
 
 # Create a new data frame that has all of the countries based off of population data availability
@@ -37,8 +36,12 @@ country_indicators <- country_indicators %>% select(-c(date, obs_status, footnot
 
 # Add indicator information to dataframe
 for (i in seq(1, length(my_indicators))){
-  indicator_data <- wb_data(my_indicators[i], country = "countries_only", mrnev = 1, freq = "Y")
-  country_indicators <- merge(country_indicators, indicator_data[c(3,5)], by = "country", all.x = TRUE)
+  try({
+    indicator_data <- wb_data(my_indicators[i], country = "countries_only", mrnev = 1, freq = "Y")
+    country_indicators <- merge(country_indicators, indicator_data[c(3,5)], by = "country", all.x = TRUE)
+  }, silent = F)
+  
+  Sys.sleep(2)
 }
 
 # Add region information to dataframe
@@ -48,7 +51,6 @@ country_indicators <- merge(country_indicators, wb_countries[c("country","region
 ##### Gather information from COVID-19 API
 
 # Get confirmed COVID-19 for all countries
-
 confirmed_cases <- GET('https://covid19-stats-api.herokuapp.com/api/v1/cases/country/confirmed')
 confirmed_text <- content(confirmed_cases, "text")
 confirmed_json <- fromJSON(confirmed_text, flatten = T) 
@@ -68,15 +70,14 @@ deaths_cases <- GET('https://covid19-stats-api.herokuapp.com/api/v1/cases/countr
 deaths_text <- content(deaths_cases, "text")
 deaths_json <- fromJSON(deaths_text, flatten = T) 
 deaths_df <- as.data.frame(deaths_json)
-deaths_df <- dplyr::select(deaths_df, -country)
 
-
-# Renme counts to deaths 
+# Rename counts to deaths 
 deaths_df <- dplyr::rename(deaths_df, deaths = count)
 
 # Retrieve iso3c codes and add as new column
 deaths_df <- dplyr::mutate(deaths_df, iso3c = countrycode::countryname(deaths_df$country, destination = "iso3c"))
 deaths_df["iso3c"][deaths_df["country"] == "Micronesia"] <- "FSM"
+deaths_df <- dplyr::select(deaths_df, -country)
 
 ##### Merge COVID and WorldBank data based on iso3c
 country_info <- merge(country_indicators, confirmed_df, by = "iso3c", all.x = T)
@@ -90,7 +91,7 @@ country_info <- country_info %>%
   mutate(mortality_rate = round((deaths / population) * 100000, 2))
                 
 
-
+saveRDS(country_info, file = "country.info.RDS")
 
 
 
